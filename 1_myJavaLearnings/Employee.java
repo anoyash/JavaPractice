abd6aa3f5ba4f92ef3e8d7614c26e44408390633
Ashutosh Kumar <ak2417942@gmail.com>
2025-09-07 22:02:29 +0530

    added Employee Class

---
+++ 1_myJavaLearnings/Employee.java
@@ -0,0 +1,7 @@
+public class Employee {
+    public static void main(String[] args) {
+        
+        int a = 1;
+        System.out.println("This is my first program and value of a : "+a);
+    }
+}
