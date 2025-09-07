61d60c4ef847c9dddf035a34687ed92bab62f54d
Ashutosh Kumar <ak2417942@gmail.com>
2025-09-07 22:55:19 +0530

    Individual Class added

--- 1_myJavaLearnings/Employee.java
+++ 1_myJavaLearnings/Employee.java
@@ -1,7 +1,16 @@
 public class Employee {
+    byte byteVar;
+    short shortVar;
+    int intVar;
+    long longVar;
+
     public static void main(String[] args) {
-        
+
         int a = 1;
-        System.out.println("This is my first program and value of a : "+a);
+        System.out.println("This is my first program and value of a : " + a);
+
+        for (char b = 97; b <= 122; b++) {
+            System.out.print((b + " "));
+        }
     }
 }
---
+++ 1_myJavaLearnings/Individual.java
@@ -0,0 +1,10 @@
+public class Individual {
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        System.out.println("Integer Default Value : " + e1.intVar);
+        System.out.println("Bye Default Value     : " + e1.byteVar);
+        System.out.println("Short Default Value   : " + e1.shortVar);
+        System.out.println("Long Default Value    : " + e1.longVar);
+
+    }
+}
