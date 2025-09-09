7e2cff3fe20c350e4da21d615b1d51f9ef574b9b
Ashutosh Kumar <ak2417942@gmail.com>
2025-09-09 14:09:37 +0530

    reference practice added and String prac as well

--- 2_practice/Student.java
+++ 2_practice/Student.java
@@ -12,8 +12,25 @@ public class Student {
         Student myStudent2 = new Student(1, 12, "Ashutosh", mySubjects);
 
         myStudent1.getStudent();
-        System.err.println("\n");
+        System.out.println("\n");
         myStudent2.getStudent();
+        System.out.println("\n");
+
+        String myName = "Ashutosh";
+        String myFullName = myName;
+        myFullName = "Ashutosh Kumar";
+        System.out.println("myName  : " + myName + " :: myFullName : " + myFullName);
+
+        String myName1 = new String("Ashu");
+        String myFullName1 = new String();
+        myFullName1 = myName1;
+        myFullName1 = "Tosh";
+        System.out.println("myFullNam1 : " + myName1);
+
+        Integer myNum = 10;
+        Integer newNum = myNum; // this is a reference creation
+        newNum = 12; // this will change the value in heap memory ,
+        System.out.println("Value of newNum :" + newNum);
     }
 
     Student() {
@@ -29,8 +46,21 @@ public class Student {
         this.subjects = ipSubjects;
     }
 
-    public void setStudent(int ipRoll, int ipClassNum, String ipName) {
+    /*
+     * This below method is not a constructor , Contructor does not have return
+     * type.
+     */
+    public void Student(int ipRoll, int ipClassNum, String ipName, String[] ipSubjects) {
+        rollNum = ipRoll;
+        classNum = ipClassNum;
+        name = ipName;
+        this.subjects = ipSubjects;
+    }
 
+    public void setStudent(int ipRoll, int ipClassNum, String ipName) {
+        rollNum = ipRoll;
+        classNum = ipClassNum;
+        name = ipName;
     }
 
     public void getStudent() {
