536c6cb1e04631b915d01ee049f5de81cc0d9382
Ashutosh Kumar <ak2417942@gmail.com>
2025-09-16 13:36:06 +0530

    eager initialization completed , lazy initialization started

--- src/3_Class_pkg/MySingletonClass.java
+++ src/3_Class_pkg/MySingletonClass.java
@@ -1,12 +1,22 @@
-public class MySingletonClass {
 
+// 1st : Below is the implementation of eager initialization
+// public class MySingletonClass {
 
-    private static MySingletonClass dbConnection = new MySingletonClass();
+// /* This is eager initialization */
+// private static MySingletonClass dbConnection = new MySingletonClass();
 
-    private  MySingletonClass(){
-    }
+// private MySingletonClass() {
+// System.out.println("constructor is called ");
+// }
 
-    public static MySingletonClass getDbConnection(){
-        return dbConnection;
-    }
-}
+// public static MySingletonClass getDbConnection() {
+// return dbConnection;
+// }
+
+// public static void main(String args[]) {
+// MySingletonClass myDbConnect = MySingletonClass.getDbConnection();
+// System.out.println(myDbConnect);
+// }
+// }
+
+// 2nd : Implementation of Lazy initialization
