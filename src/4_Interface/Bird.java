8e5e7633180af244cab90996a2915c515c4cbc01
Ashutosh Kumar <ak2417942@gmail.com>
2025-09-22 23:50:52 +0530

    interface class updated

--- src/4_Interface/Bird.java
+++ src/4_Interface/Bird.java
@@ -2,13 +2,35 @@ public class Bird {
 
     public static void main(String args[]) {
         System.out.println("Interface demonstration has been started ");
+        BirdTemplate myBird1 = new Eagle();
+        myBird1.fly();
     }
 }
 
 interface BirdTemplate {
-    void fly();
+    public void fly();
 
-    void eat();
+    public void eat();
 
-    void sleep();
+    public void sleep();
+}
+
+interface BirdTemplate2 extends BirdTemplate {
+    void fly2();
+}
+
+class Eagle implements BirdTemplate {
+
+    public void fly() {
+        System.out.println("Eagle is flying ");
+    }
+
+    public void eat() {
+        System.out.println("Eagle is eating");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Eagle is sleeping");
+    }
 }
