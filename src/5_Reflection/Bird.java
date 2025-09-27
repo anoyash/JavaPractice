dc0f1780acbd2d7a9eed5a2f7eb18556aa9146cd
Ashutosh Kumar <ak2417942@gmail.com>
2025-09-27 20:07:27 +0530

    added example of reflection and modification of data members

--- src/5_Reflection/Bird.java
+++ src/5_Reflection/Bird.java
@@ -1,7 +1,10 @@
+import java.lang.reflect.Field;
+
 public class Bird {
 
     public String breed;
     public String color;
+    private String birdClass;
 
     public void setBird(String ipBreed, String ipColor) {
         this.breed = ipBreed;
@@ -12,7 +15,11 @@ public class Bird {
         System.out.println("This is a private method for the class bird");
     }
 
-    public static void main(String args[]) {
+    public void getBird() {
+        System.out.println("Bird data is : " + this.breed + " : " + this.color + " " + this.birdClass);
+    }
+
+    public static void main(String args[]) throws NoSuchFieldException, IllegalAccessException {
         // 1. Way of creating an instance of Class , Class object
         Class myClassObj1 = Bird.class;
 
@@ -25,11 +32,31 @@ public class Bird {
         Bird myBirdObj = new Bird();
         Class myClassObj3 = myBirdObj.getClass();
 
-        // 2. Doing Relection of Class
+        // 2. Doing Relection of Class data members
         Class myClassObj4 = Bird.class;
         System.out.println(myClassObj4.getName());
         System.out.println(myClassObj4.getModifiers());
+        System.out.println(myClassObj4.getFields());
 
+        // getFields only return public method of the class under examination and all
+        // its super class and interfaces
+        Field[] myClassFields1 = myClassObj4.getFields();
+        for (Field myField : myClassFields1) {
+            // here myField is an object
+            System.out.println(myField);
+        }
+
+        Field[] myClassFields2 = myClassObj4.getDeclaredFields();
+        for (Field myField : myClassFields2) {
+            System.out.println(myField.getName());
+        }
+
+        Field myField1 = myClassObj4.getDeclaredField("breed");
+        Bird myBirdObj2 = new Bird();
+        myField1.set(myBirdObj2, "brown king");
+        myBirdObj2.getBird();
+
+        // 3. Reflection of Class methods
     }
 
 }
