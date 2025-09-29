e22bdbad19f71c1849682eaf506c54f61b3eefe1
Ashutosh Kumar <ak2417942@gmail.com>
2025-09-29 13:23:14 +0530

    exception handling practice

---
+++ src/7_ExceptionHandling/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Exception :
+ * 1. Un-checked / Run time exception
+ * 2. Checked / Compile time exception
+ * 
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
