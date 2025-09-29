28cfcbaf63ee68548b0a700c20b698a7dba97d4e
Ashutosh Kumar <ak2417942@gmail.com>
2025-09-29 13:37:59 +0530

    runtime exceptions : arithmetic exception and class cast exception

---
+++ src/7_ExceptionHandling/Bird.java
@@ -0,0 +1,15 @@
+public class Bird {
+
+    public static void main(String args[]) {
+        // 1. Arithmetic exception
+        // method1();
+
+        // 2. Class cast exception : java.lang.ClassCastException
+        // Object val = 0;
+        // System.out.println((String) val);
+    }
+
+    public static void method1() {
+        throw new ArithmeticException("Exception ho gya Ashutosh bhai..");
+    }
+}
