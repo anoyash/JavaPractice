e660221a1bb6e46d24ba1ead1ff7580d31f39734
Ashutosh Kumar <ak2417942@gmail.com>
2025-09-29 13:42:47 +0530

    exceptions runtime

--- src/7_ExceptionHandling/Bird.java
+++ src/7_ExceptionHandling/Bird.java
@@ -7,6 +7,15 @@ public class Bird {
         // 2. Class cast exception : java.lang.ClassCastException
         // Object val = 0;
         // System.out.println((String) val);
+
+        // 3. Array index out of bounds exception :
+        // java.lang.ArrayIndexOutOfBoundsException
+        // Integer[] myArray = new Integer[5];
+        // System.out.println(myArray[1] + " " + myArray[5]);
+
+        String val = null;
+        System.out.println(val);
+        System.out.println(val.charAt(0));
     }
 
     public static void method1() {
