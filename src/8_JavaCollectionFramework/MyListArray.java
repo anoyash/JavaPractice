169f5ed30eb014b1ae67be07d38f4efc01cdff78
Ashutosh Kumar <ak2417942@gmail.com>
2025-11-25 12:39:25 +0530

    adding the new Collections class example

--- src/8_JavaCollectionFramework/MyListArray.java
+++ src/8_JavaCollectionFramework/MyListArray.java
@@ -1,10 +1,19 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
 public class MyListArray {
 
     public static void main(String[] args) {
+
+        method1();
+        method2();
+    }
+
+    public static void method1() {
+        System.out.println("Method 1st");
+
         List<Integer> values = new ArrayList<>();
 
         for (int i = 1; i <= 4; i++)
@@ -21,5 +30,43 @@ public class MyListArray {
                 valueIterator.remove();
         }
 
+        System.out.println("Iterating the values using for-each Loop");
+
+        for (int val : values) {
+            System.out.println(val);
+        }
+
+        System.out.println("Iterating the value using forEach method");
+
+        values.forEach((Integer val) -> {
+            System.out.println(val);
+        });
+
+    }
+
+    public static void method2() {
+        /*
+         * Collections : max , min , reverse , sort , swap , copy , rotate ,
+         * binanrySearch , get , unmodifiableCollection , suffle
+         */
+
+        System.out.println("Method 2nd");
+
+        List<Integer> values = new ArrayList<>();
+
+        for (int i = 1; i <= 4; i++)
+            values.add(i);
+
+        System.out.println("Maximum Value in values : " + Collections.max(values));
+        System.out.println("Minimum Value in values : " + Collections.min(values));
+
+        System.out.println("Reversing the values");
+        Collections.reverse(values);
+
+        values.forEach((Integer val) -> System.out.print(val + " : "));
+
+        System.out.println("\n" + "After Swapping 1st element and 4th element");
+        Collections.swap(values, 0, 3);
+        values.forEach((Integer val) -> System.out.print(val + " : "));
     }
 }
